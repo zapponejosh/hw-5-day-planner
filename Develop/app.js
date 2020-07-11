5fef8425e2745bf7cf6b2c322d96d4c8f97d3094
Josh Zappone <zappone.josh@gmail.com>
2020-07-10 18:11:26 -0700

    adds unsaved pseudocode and a quick media query fix

--- Develop/app.js
+++ Develop/app.js
@@ -1,7 +1,19 @@
 
 
-// Save btn when not saved <i class="fas fa-save fa-lg"></i>
-// Save btn when saved <i class="fas fa-pen-square"></i>
+/* 
+on page load the agenda input divs get content from local storage
+text divs are empty and user clicks on the pencil and is able to edit the text. 
+user clicks save icon and text content update local storage
+
+
+TIME
+If the time has passed a class of "past" is added to the td of the applicable time (time is checked on load and save/edit)
+if in the present hour the td has a class of "present" 
+if it is a future hour the td has a class of "future"
+
+The date is also stored in localStorage and if the date is not today the localstorage is cleared the the date updated
+
+*/
 
 
 $(document).ready(function () {
